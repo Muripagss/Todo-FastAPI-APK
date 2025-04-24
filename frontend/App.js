1ca7f695cb4f65da2bf1aae84a476f1c08364405
muripagss <rmuripaga25@gmail.com>
2025-04-24 15:46:04 +0800

    FastAPI-APK

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://todo-fastapi-apk.onrender.com"; 
 
 const lightModeStyles = {
   container: {
